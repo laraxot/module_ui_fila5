65288f06d04c09bcdeba7cd2d7b0305d121ef27f
marco76tv <marco.sottana@gmail.com>
2026-04-15 13:45:36 +0200

    .

---
+++ database/factories/CollectionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\UI\Models\Collection;
+
+class CollectionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     */
+    protected $model = Collection::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [];
+    }
+}
---
+++ docs/roadmap/legacy-roadmap.md
@@ -0,0 +1,3 @@
+# UI Module Roadmap
+
+Vedi [roadmap/00-index](roadmap/00-index.md).
