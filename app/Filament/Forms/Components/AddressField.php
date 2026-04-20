6d3a3268ea9bc80b1960cae68cfc9ebfbdf3464a
marco76tv <marco.sottana@gmail.com>
2026-04-20 21:37:12 +0200

    .

--- app/Filament/Forms/Components/AddressField.php
+++ app/Filament/Forms/Components/AddressField.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Forms\Components;
 
-use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Grid;
@@ -12,11 +11,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Modules\Xot\Filament\Forms\Components\XotBaseField;
 use Webmozart\Assert\Assert;
 
 // use Squire\Models\Country;
 
-class AddressField extends Field
+class AddressField extends XotBaseField
 {
     /** @var string|callable|null */
     public $relationship;
