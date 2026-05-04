fb1365d1ff76c9879134c9ba2bfef03d62bedb0d
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-05-04 20:18:36 +0000

    Lint

--- tests/Unit/Widgets/BaseCalendarWidgetTest.php
+++ tests/Unit/Widgets/BaseCalendarWidgetTest.php
@@ -13,8 +13,7 @@ use Modules\UI\Filament\Widgets\UserCalendarWidget;
 use Modules\UI\Tests\TestCase;
 
 beforeEach(function () {
-    $this->widget = new class extends UserCalendarWidget
-    {
+    $this->widget = new class extends UserCalendarWidget {
         public function getActionName(string $function): string
         {
             unset($function);
