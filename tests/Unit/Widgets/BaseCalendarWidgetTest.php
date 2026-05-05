fec723989553575c5b85ddc7fe71c77d856ef9fd
Malebestia <malebestia@yahoo.it>
2026-05-05 09:44:58 +0200

    .

--- tests/Unit/Widgets/BaseCalendarWidgetTest.php
+++ tests/Unit/Widgets/BaseCalendarWidgetTest.php
@@ -13,7 +13,8 @@ use Modules\UI\Filament\Widgets\UserCalendarWidget;
 use Modules\UI\Tests\TestCase;
 
 beforeEach(function () {
-    $this->widget = new class extends UserCalendarWidget {
+    $this->widget = new class extends UserCalendarWidget
+    {
         public function getActionName(string $function): string
         {
             unset($function);
