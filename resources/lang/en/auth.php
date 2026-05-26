38476b1f4a4b8913d765908af58fbb6f5706b40b
marco76tv <marco.sottana@gmail.com>
2026-05-26 21:09:57 +0200

    .

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,34 +1,35 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'login' => [
-        'title' => 'Sign in',
-        'email' => 'Email',
-        'password' => 'Password',
-        'remember_me' => 'Remember me',
-        'forgot_password' => 'Forgot your password?',
-        'submit' => 'Sign in',
-        'or' => 'or',
-        'create_account' => 'create an account',
-    ],
-    'register' => [
-        'title' => 'Register',
-        'submit' => 'Register',
-    ],
-    'logout' => [
-        'title' => 'Logout',
-        'confirm_message' => 'Are you sure you want to logout?',
-        'confirm_button' => 'Confirm Logout',
-        'cancel_button' => 'Cancel',
-        'success_title' => 'Logged Out',
-        'success_message' => 'You have been successfully logged out.',
-        'error_title' => 'Logout Error',
-        'error_message' => 'An error occurred during logout.',
-        'try_again' => 'Try Again',
-        'back_to_home' => 'Back to Home',
-    ],
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-];
+|<|?|p|h|p|
+|
+|d|e|c|l|a|r|e|(|s|t|r|i|c|t|_|t|y|p|e|s|=|1|)|;|
+|
+|r|e|t|u|r|n| |[|
+| | | | |'|l|o|g|i|n|'| |=|>| |[|
+| | | | | | | | |'|t|i|t|l|e|'| |=|>| |'|S|i|g|n| |i|n|'|,|
+| | | | | | | | |'|e|m|a|i|l|'| |=|>| |'|E|m|a|i|l|'|,|
+| | | | | | | | |'|p|a|s|s|w|o|r|d|'| |=|>| |'|P|a|s|s|w|o|r|d|'|,|
+| | | | | | | | |'|r|e|m|e|m|b|e|r|_|m|e|'| |=|>| |'|R|e|m|e|m|b|e|r| |m|e|'|,|
+| | | | | | | | |'|f|o|r|g|o|t|_|p|a|s|s|w|o|r|d|'| |=|>| |'|F|o|r|g|o|t| |y|o|u|r| |p|a|s|s|w|o|r|d|?|'|,|
+| | | | | | | | |'|s|u|b|m|i|t|'| |=|>| |'|S|i|g|n| |i|n|'|,|
+| | | | | | | | |'|o|r|'| |=|>| |'|o|r|'|,|
+| | | | | | | | |'|c|r|e|a|t|e|_|a|c|c|o|u|n|t|'| |=|>| |'|c|r|e|a|t|e| |a|n| |a|c|c|o|u|n|t|'|,|
+| | | | |]|,|
+| | | | |'|r|e|g|i|s|t|e|r|'| |=|>| |[|
+| | | | | | | | |'|t|i|t|l|e|'| |=|>| |'|R|e|g|i|s|t|e|r|'|,|
+| | | | | | | | |'|s|u|b|m|i|t|'| |=|>| |'|R|e|g|i|s|t|e|r|'|,|
+| | | | |]|,|
+| | | | |'|l|o|g|o|u|t|'| |=|>| |[|
+| | | | | | | | |'|t|i|t|l|e|'| |=|>| |'|L|o|g|o|u|t|'|,|
+| | | | | | | | |'|c|o|n|f|i|r|m|_|m|e|s|s|a|g|e|'| |=|>| |'|A|r|e| |y|o|u| |s|u|r|e| |y|o|u| |w|a|n|t| |t|o| |l|o|g|o|u|t|?|'|,|
+| | | | | | | | |'|c|o|n|f|i|r|m|_|b|u|t|t|o|n|'| |=|>| |'|C|o|n|f|i|r|m| |L|o|g|o|u|t|'|,|
+| | | | | | | | |'|c|a|n|c|e|l|_|b|u|t|t|o|n|'| |=|>| |'|C|a|n|c|e|l|'|,|
+| | | | | | | | |'|s|u|c|c|e|s|s|_|t|i|t|l|e|'| |=|>| |'|L|o|g|g|e|d| |O|u|t|'|,|
+| | | | | | | | |'|s|u|c|c|e|s|s|_|m|e|s|s|a|g|e|'| |=|>| |'|Y|o|u| |h|a|v|e| |b|e|e|n| |s|u|c|c|e|s|s|f|u|l|l|y| |l|o|g|g|e|d| |o|u|t|.|'|,|
+| | | | | | | | |'|e|r|r|o|r|_|t|i|t|l|e|'| |=|>| |'|L|o|g|o|u|t| |E|r|r|o|r|'|,|
+| | | | | | | | |'|e|r|r|o|r|_|m|e|s|s|a|g|e|'| |=|>| |'|A|n| |e|r|r|o|r| |o|c|c|u|r|r|e|d| |d|u|r|i|n|g| |l|o|g|o|u|t|.|'|,|
+| | | | | | | | |'|t|r|y|_|a|g|a|i|n|'| |=|>| |'|T|r|y| |A|g|a|i|n|'|,|
+| | | | | | | | |'|b|a|c|k|_|t|o|_|h|o|m|e|'| |=|>| |'|B|a|c|k| |t|o| |H|o|m|e|'|,|
+| | | | |]|,|
+| | | | |'|f|a|i|l|e|d|'| |=|>| |'|T|h|e|s|e| |c|r|e|d|e|n|t|i|a|l|s| |d|o| |n|o|t| |m|a|t|c|h| |o|u|r| |r|e|c|o|r|d|s|.|'|,|
+| | | | |'|t|h|r|o|t|t|l|e|'| |=|>| |'|T|o|o| |m|a|n|y| |l|o|g|i|n| |a|t|t|e|m|p|t|s|.| |P|l|e|a|s|e| |t|r|y| |a|g|a|i|n| |i|n| |:|s|e|c|o|n|d|s| |s|e|c|o|n|d|s|.|'|,|
+|]|;|
+|
