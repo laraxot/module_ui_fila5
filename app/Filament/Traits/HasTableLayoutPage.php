6158b5db75053cdc83d335fd7d0d9bfaf40e16f0
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-06 09:08:38 +0000

    Lint

--- app/Filament/Traits/HasTableLayoutPage.php
+++ app/Filament/Traits/HasTableLayoutPage.php
@@ -19,7 +19,7 @@ trait HasTableLayoutPage
     public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
 
     public function mountTableLayoutFromSession(
-        string $identifier = 'default'
+        string $identifier = 'default',
     ): void {
         $this->layoutView = $this->getCurrentLayout($identifier);
     }
@@ -43,21 +43,21 @@ trait HasTableLayoutPage
         }
 
         return (function (): TableLayoutEnum {
-            /** @phpstan-var object{layoutView: TableLayoutEnum} $this */
+            /* @phpstan-var object{layoutView: TableLayoutEnum} $this */
             return $this->layoutView;
         })->call($livewire);
     }
 
     public static function applyLayoutTo(
         object $livewire,
-        TableLayoutEnum $layout
+        TableLayoutEnum $layout,
     ): void {
         if (! self::isLayoutCapable($livewire)) {
             return;
         }
 
         (function (TableLayoutEnum $layout): void {
-            /** @phpstan-var object{layoutView: TableLayoutEnum} $this */
+            /* @phpstan-var object{layoutView: TableLayoutEnum} $this */
             // @phpstan-ignore assign.propertyReadOnly
             $this->layoutView = $layout;
         })->call($livewire, $layout);
