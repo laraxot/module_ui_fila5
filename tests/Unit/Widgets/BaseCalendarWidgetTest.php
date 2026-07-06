00af7f6c4c3e9aa308aa7d9b51b8277337d3938f
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-06 12:50:42 +0000

    Lint

--- tests/Unit/Widgets/BaseCalendarWidgetTest.php
+++ tests/Unit/Widgets/BaseCalendarWidgetTest.php
@@ -32,8 +32,8 @@ function createTestCalendarWidget(): UserCalendarWidget
 
 beforeEach(function (): void {
     /* @var \Modules\UI\Tests\TestCase $this */
-    /** @var \Modules\UI\Tests\TestCase $this */
-        $this->mockService(SaveTransAction::class, static function (MockInterface $mock): void {
+    /* @var \Modules\UI\Tests\TestCase $this */
+    $this->mockService(SaveTransAction::class, static function (MockInterface $mock): void {
         /** @var ExpectationInterface $expectation */
         $expectation = $mock->shouldReceive('execute');
         $expectation->andReturn(null);
