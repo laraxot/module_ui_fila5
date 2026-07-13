44b099f60773045e660d6170dec48ab5d3c7475a
marco76tv <marco.sottana@gmail.com>
2026-07-13 22:34:19 +0200

    .

--- lang/it/enum_select.php
+++ lang/it/enum_select.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 return [
     'fields' => [
-        'type_id' => [
-            'label' => 'type_id',
-            'placeholder' => 'type_id',
-            'helper_text' => 'type_id',
-            'description' => 'type_id',
-        ],
+        'type_id' => ['label' => 'type_id', 'placeholder' => 'type_id', 'helper_text' => 'type_id', 'description' => 'type_id'],
+        'enum' => ['label' => 'enum', 'placeholder' => 'enum', 'helper_text' => 'enum', 'description' => 'enum'],
     ],
 ];
