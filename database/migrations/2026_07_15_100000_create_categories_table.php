263db960c71b4bfbfeff0980468189d71d15d06a
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-15 13:11:50 +0000

    Lint

--- database/migrations/2026_07_15_100000_create_categories_table.php
+++ database/migrations/2026_07_15_100000_create_categories_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Modules\UI\Models\Category;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
-return new class extends XotBaseMigration
-{
+return new class extends XotBaseMigration {
     protected ?string $model_class = Category::class;
 
     public function up(): void
