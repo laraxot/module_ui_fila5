e351fbe361eea6186f8f113386b255b8e1232e53
marco76tv <marco.sottana@gmail.com>
2026-07-22 21:49:08 +0200

    fix(UI): risolve marker di conflitto Git in app/Filament/Forms/Components/Children.php

--- app/Filament/Forms/Components/Children.php
+++ app/Filament/Forms/Components/Children.php
@@ -4,25 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Forms\Components;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> dfbb8305 (.)
 use Modules\Xot\Filament\Forms\Components\XotBaseViewField;
 
 // use Filament\Support\Components\ViewComponent;
 
 final class Children extends XotBaseViewField
-<<<<<<< HEAD
-=======
-use Filament\Forms\Components\ViewField;
-
-// use Filament\Support\Components\ViewComponent;
-
-final class Children extends ViewField
->>>>>>> dfac49d (.)
-=======
->>>>>>> dfbb8305 (.)
 {
     protected string $view = 'ui::filament.forms.components.navigation-builder';
 
