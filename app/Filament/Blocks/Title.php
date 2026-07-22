6bd59bf21ea30b7ff9abaf2d094d10ed2ba5701f
marco76tv <marco.sottana@gmail.com>
2026-07-22 21:49:48 +0200

    fix(UI): risolve marker di conflitto Git in app/Filament/Blocks/Title.php

--- app/Filament/Blocks/Title.php
+++ app/Filament/Blocks/Title.php
@@ -10,37 +10,6 @@ use Filament\Forms\Components\TextInput;
 use Modules\Xot\Actions\Filament\Block\GetViewBlocksOptionsByTypeAction;
 
 class Title // extends XotBaseBlock
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> dfbb8305 (.)
-{public static function make(string $name = 'title', string $context = 'form'): Block
-{
-    // $view = 'ui::components.blocks.title.v1';
-    // $views = app(GetViewsSiblingsAndSelfAction::class)->execute($view);
-
-    $options = app(GetViewBlocksOptionsByTypeAction::class)->execute('title', false);
-
-    return Block::make($name)
-        ->schema([
-            TextInput::make('text')->required(),
-            Select::make('level')
-                ->options([
-                    'h2' => 'h2',
-                    'h3' => 'h3',
-                    'h4' => 'h4',
-                ])
-                ->afterStateHydrated(static function (TextInput $component, mixed $state): void {
-                    if (null === $state || '' === $state) {
-                        $component->state('h2');
-                    }
-                }),
-            Select::make('view')->options(is_array($options) ? $options : []),
-        ])
-        ->columns('form' === $context ? 2 : 1);
-}
-<<<<<<< HEAD
-=======
 {
     public static function make(string $name = 'title', string $context = 'form'): Block
     {
@@ -59,15 +28,12 @@ class Title // extends XotBaseBlock
                         'h4' => 'h4',
                     ])
                     ->afterStateHydrated(static function (TextInput $component, mixed $state): void {
-                        if ($state === null || $state === '') {
+                        if (null === $state || '' === $state) {
                             $component->state('h2');
                         }
                     }),
                 Select::make('view')->options(is_array($options) ? $options : []),
             ])
-            ->columns($context === 'form' ? 2 : 1);
+            ->columns('form' === $context ? 2 : 1);
     }
->>>>>>> dfac49d (.)
-=======
->>>>>>> dfbb8305 (.)
 }
