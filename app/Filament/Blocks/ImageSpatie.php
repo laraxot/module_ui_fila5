a05a93e1536937053466cb596b700005551c576a
marco76tv <marco.sottana@gmail.com>
2026-07-22 21:48:27 +0200

    fix(UI): risolve marker di conflitto Git in app/Filament/Blocks/ImageSpatie.php

--- app/Filament/Blocks/ImageSpatie.php
+++ app/Filament/Blocks/ImageSpatie.php
@@ -23,15 +23,7 @@ final class ImageSpatie
             ->schema([
                 Hidden::make('img_uuid')
                     ->default(Str::uuid()->toString(...))
-<<<<<<< HEAD
-<<<<<<< HEAD
                     ->formatStateUsing(fn ($state) => $state ?? Str::uuid()->toString()),
-=======
-                    ->formatStateUsing(static fn ($state) => $state ?? Str::uuid()->toString()),
->>>>>>> dfac49d (.)
-=======
-                    ->formatStateUsing(fn ($state) => $state ?? Str::uuid()->toString()),
->>>>>>> dfbb8305 (.)
                 // ->live()
                 SpatieMediaLibraryFileUpload::make('image')
                     ->live()
@@ -47,18 +39,8 @@ final class ImageSpatie
                     ->openable()
                     ->downloadable()
                     // ->rules(Rule::dimensions()->maxWidth(600)->maxHeight(800))
-<<<<<<< HEAD
-<<<<<<< HEAD
                     ->collection(fn (Get $get) => $get('img_uuid'))
                     ->afterStateUpdated(function (
-=======
-                    ->collection(static fn (Get $get) => $get('img_uuid'))
-                    ->afterStateUpdated(static function (
->>>>>>> dfac49d (.)
-=======
-                    ->collection(fn (Get $get) => $get('img_uuid'))
-                    ->afterStateUpdated(function (
->>>>>>> dfbb8305 (.)
                         HasForms $_livewire,
                         SpatieMediaLibraryFileUpload $_component,
                         TemporaryUploadedFile $state,
@@ -75,14 +57,6 @@ final class ImageSpatie
                     }),
                 TextInput::make('caption'),
             ])
-<<<<<<< HEAD
-<<<<<<< HEAD
             ->columns('form' === $context ? 2 : 1);
-=======
-            ->columns($context === 'form' ? 2 : 1);
->>>>>>> dfac49d (.)
-=======
-            ->columns('form' === $context ? 2 : 1);
->>>>>>> dfbb8305 (.)
     }
 }
