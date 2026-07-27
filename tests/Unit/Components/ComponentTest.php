98cc7bf523d44c2d9c22fde2b7f7891715755836
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-27 15:39:59 +0000

    Lint

--- tests/Unit/Components/ComponentTest.php
+++ tests/Unit/Components/ComponentTest.php
@@ -13,8 +13,7 @@ uses(TestCase::class);
 
 describe('Component', function (): void {
     test('ui components can be rendered', function (): void {
-        $component = new class() extends Component
-        {
+        $component = new class extends Component {
             public function render(): View
             {
                 return view('ui::components.ui.button');
