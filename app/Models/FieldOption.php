b9978b5124b523a9d68212bd9f3125193a1b0fe1
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-08-05 21:38:23 +0000

    Lint

--- app/Models/FieldOption.php
+++ app/Models/FieldOption.php
@@ -17,11 +17,12 @@ use Modules\Xot\Models\BaseModel;
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
  *
-<<<<<<< HEAD
+ * <<<<<<< HEAD
+ *
  * @method static FieldOptionFactory          factory($count = null, $state = [])
-=======
- * @method static FieldOptionFactory factory($count = null, $state = [])
->>>>>>> 6e44b7d5 (.)
+ *                                                                                =======
+ * @method static FieldOptionFactory          factory($count = null, $state = [])
+ *                                                                                >>>>>>> 6e44b7d5 (.)
  * @method static Builder<static>|FieldOption newModelQuery()
  * @method static Builder<static>|FieldOption newQuery()
  * @method static Builder<static>|FieldOption query()
