d0af79a8816fa9e6662f4949cbb1b85dedebeef7
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-08-18 19:35:04 +0000

    Lint

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -40,11 +40,12 @@ use Modules\Xot\Models\BaseModel;
  * @method static Builder<static>|Collection newQuery()
  * @method static Builder<static>|Collection query()
  *
-<<<<<<< .merge_file_3FKOja
-=======
+ * <<<<<<< .merge_file_3FKOja
+ * =======
  * <<<<<<< .merge_file_hwLZ8g
  * >>>>>>> .merge_file_NIoM7l
->>>>>>> .merge_file_guFVHX
+ * >>>>>>> .merge_file_guFVHX
+ *
  * @mixin \Eloquent
  */
 class Collection extends BaseModel
