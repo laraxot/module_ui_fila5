5c5218e900cebc0f88ac03c37224477cd53b84b7
Marco Xot <marco76tv@hotmail.com>
2026-09-05 16:44:09 +0200

    .

--- tests/Unit/UiBasePolicyBehaviorTest.php
+++ tests/Unit/UiBasePolicyBehaviorTest.php
@@ -21,13 +21,9 @@ function uiBehaviorUser(array $roles = []): UserContract
 {
     /** @var MockInterface&UserContract $user */
     $user = Mockery::mock(UserContract::class);
-    TestCase::expectMethod($user, 'hasRole')
-        ->andReturnUsing(static function (array|string $richiesti) use ($roles): bool {
-            /** @var list<string> $normalizzati */
-            $normalizzati = is_array($richiesti) ? $richiesti : [$richiesti];
-
-            return array_intersect($normalizzati, $roles) !== [];
-        });
+    $user->allows([
+        'hasRole' => static fn (string $richiesto): bool => in_array($richiesto, $roles, true),
+    ]);
 
     return $user;
 }
