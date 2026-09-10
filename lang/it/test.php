6ca581fcaa6fc91b45d389724d4bd0c54f507256
Marco Xot <marco76tv@hotmail.com>
2026-09-10 19:07:39 +0200

    .

--- lang/it/test.php
+++ lang/it/test.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'navigation' => ['label' => 'test.navigation', 'group' => 'test.navigation', 'icon' => 'test.navigation', 'sort' => 48],
+    'navigation' => ['label' => 'Test', 'group' => 'Test', 'icon' => 'Test', 'sort' => 48],
     'actions' => [
         'save' => ['label' => 'save', 'icon' => 'save', 'tooltip' => 'save'],
     ],
